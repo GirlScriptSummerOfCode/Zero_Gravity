551f6469a6f85e790b7e743647a780cc8abf0e20
adarshlilha <adarshlilha@gmail.com>
2018-07-02 14:35:59 +0530

    add-gatsby-boilerplate

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Zero Gravity',
+  },
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+};
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => (
+  <div className="header">
+    <p>
+        Welcome to Zero Gravity
+    </p>
+  </div>
+);
+
+export default Header;
